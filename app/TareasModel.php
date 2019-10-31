66b7fc66fe24559be33a386d5b690fbdf844f7ec
joseleonardo1995 <leonardosabando03@hotmail.com>
2019-10-31 12:51:30 -0500

    CAMBIOS

--- app/TareasModel.php
+++ app/TareasModel.php
@@ -38,9 +38,14 @@ class TareasModel extends Model
     {
         return $this->belongsTo('App\TareasModel', 'Id_tarea','tareasIdTareas');
     }
-       public function SubTareas()
+    public function SubTareasPendientes()
     {
-        return $this->hasMany('App\TareasModel', 'tareasIdTareas','Id_tarea')->with('Responsables','Participantes','Observadores','Usuario','SubTareas','Observacion','TipoTareas');
+        return $this->hasMany('App\TareasModel', 'tareasIdTareas','Id_tarea')->where('Estado_Tarea','Pendiente')->with('Responsables','Participantes','Observadores','Usuario','SubTareas','Observacion','TipoTareas');
+    }
+
+    public function SubTareasTerminadas()
+    {
+        return $this->hasMany('App\TareasModel', 'tareasIdTareas','Id_tarea')->where('Estado_Tarea','Terminada')->with('Responsables','Participantes','Observadores','Usuario','SubTareas','Observacion','TipoTareas');
     }
 
     public function TipoTareas(){
